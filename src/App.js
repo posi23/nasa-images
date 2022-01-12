9e4a78e48e615821b9c88c800f7ba69a3a74eff8
posi23 <andyade23@gmail.com>
2022-01-11 18:54:14 -0600

    implemented a date picker

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@
 // import { AppProvider } from '@shopify/polaris';
 import Images from './components/Images'
 import "./App.css"
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const api = {
   key: "MXSyWvGBkfsvMYRnHQHWgPjYe9EnGD2RE4vyqVBd"
@@ -11,41 +12,87 @@ const api = {
 
 function App() {
 
-  const [images, setImages] = useState();
+  const [images, setImages] = useState(); // array of JSON data returned from the API
+  const [selectedDate, setSelectedDate] = useState("2022-01-01") // the default date to be used for the date picker
+  const [checkDate, setCheckDate] = useState(selectedDate) // the start_date parameter to be passed into the API
+  const [isLoading, setIsLoading] = useState(true) // loading state
+  const dateRef = useRef() // reference to the date picker element
 
-
-
+  // fetch data from API at the start of the website and anytime 'checkDate' changes
   useEffect(() => {
     async function getImgs() {
       try {
-        const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api.key}`)
+        const response = await fetch(`https://api.nasa.gov/planetary/apod?start_date=${checkDate}&api_key=${api.key}`)
         const data = await response.json()
         setImages(data)
-        console.log(data)
       }
       catch (err) {
         console.log(err)
       }
       finally {
-        console.log(images)
+        setIsLoading(false)
       }
     }
-
     getImgs()
-  }, [])
+
+    return () => {
+      setIsLoading(true)
+    }
+  }, [checkDate])
+
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    // condition to handle invalid date chosen on the mobile web
+    if (dateRef.current.value > dateRef.current.max || dateRef.current.value < dateRef.current.min) {
+      setSelectedDate("2022-01-01")
+      return alert(`APOD API cannot provide any images for this day: ${dateRef.current.value} \n\nPlease choose a date between ${dateRef.current.min} and ${dateRef.current.max}`)
+    }
+    setCheckDate(selectedDate)
+  }
 
 
   return (
     <div>
       <div className="App">
-        <h1>Spacestagram</h1>
+        <Router>
+          <Link to="#">
+            <h1>Spacestagram</h1>
+          </Link>
+        </Router>
+
         <p>Brought to you by NASA's Astronomy Photo of the Day (APOD) API</p>
+
+        <form>
+          <div className='label'>
+            <label htmlFor="datepicker">Choose starting date to display images from</label>
+          </div>
+          <div>
+            <input
+              ref={dateRef}
+              type="date"
+              id="datepicker"
+              value={selectedDate}
+              onChange={e => setSelectedDate(e.target.value)}
+              min="1995-06-16"
+              max={new Date().toLocaleDateString("en-CA")}
+            />
+          </div>
+          <div>
+            <button onClick={e => handleSubmit(e)}>Change Date</button>
+          </div>
+        </form>
+
+        <div className="dateinfo">
+          <h3>Showing images from <strong>{checkDate}</strong></h3>
+        </div>
       </div>
 
-      {/* <AppProvider i18n={enTranslations}> */}
-      {images && <Images images={images} />}
+      {!isLoading ? images.map(image => (
+        <Images images={image} key={image.date} />
+      )) : <p className='loading-state'>Loading...</p>}
 
-      {/* </AppProvider> */}
     </div>
   );
 }
--- src/components/Images.js
+++ src/components/Images.js
@@ -1,39 +1,55 @@
 // import { Button, Layout, Card } from '@shopify/polaris'
 import { useRef, useState } from "react";
-import img1 from "../bg.jpg"
 import "../styles/images.css"
+
 
 const Images = ({ images }) => {
 
-      const [isLiked, setIsLiked] = useState(false)
-      const likeBtn = useRef()
+      const [isLiked, setIsLiked] = useState(false) // state to show if the liked button has been pressed
+
+      const likeBtn = useRef() // reference to the like button
+      const likeAnim = useRef() // reference to the animation to be used for the like action
       function toggleLike() {
             setIsLiked(isLiked => !isLiked)
-            if (isLiked === true) likeBtn.current.className = "like-btn"
-            else likeBtn.current.className = "unlike-btn"
+            if (isLiked === true) {
+                  likeBtn.current.className = "like-btn"
+                  likeAnim.current.className = ""
+            }
+
+            else {
+                  likeBtn.current.className = "unlike-btn"
+                  likeAnim.current.className = "like-anim"
+
+                  setTimeout(() => {
+                        likeAnim.current.className = ""
+                  }, 2000);
+            }
       }
 
       return (
+            <>
 
-            <div className="main-post">
-
-
-                  <img src={images.hdurl} alt="something" />
-
-
-                  <div className='img-info'>
-                        <div>
-                              <h1>{images.title}</h1>
-                              <h1 className='date'>Date: {images.date}</h1>
+                  <div className="main-post">
+                        <div ref={likeAnim} id="like-anim" className=""></div>
+                        <div className="img-container">
+                              <img src={images.hdurl} alt="main" />
                         </div>
 
-                        <p>{images.explanation}</p>
 
-                        <button ref={likeBtn} className="like-btn" onClick={() => toggleLike()}>{isLiked === true ? "Unike" : "Like"}</button>
+
+                        <div className='img-info'>
+                              <div>
+                                    <h1>{images.title}</h1>
+                                    <h1 className='date'>Date: {images.date}</h1>
+                              </div>
+
+                              <p>{images.explanation}</p>
+
+                              <button ref={likeBtn} className="like-btn" onClick={() => toggleLike()}>{isLiked === true ? "Unike" : "Like"}</button>
+                        </div>
+
                   </div>
-
-            </div>
-
+            </>
       );
 }
 
