958efaab477946ecc24208448b71337f19bd960c
posi23 <andyade23@gmail.com>
2022-01-14 14:13:21 -0600

    removed the datepicker and created loading animation for the loading state

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 A fully responsive website/application named Spacestagram that displays images gotten from a NASA API and allows users to like or unlike each image.
 
-Developed using ReactJS for the frontend development and CSS for the styling and to develop the UI. API used is https://api.nasa.gov/planetary/apod
+Developed using ReactJS for the frontend development and CSS for the styling.
+API used is https://api.nasa.gov/planetary/apod
+
+NOTE: Website only returns images of 25 days (with the 25th day always being today's date). This is to allow the webiste successfully fetch data without user having to wait for an annoyingly long time, and to avoid connection timeout.
 
 Below is the link to the deployed website (deployed using Heroku):
 https://spacestagram23.herokuapp.com
--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Images from './components/Images'
 import "./App.css"
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 
 const api = {
@@ -10,10 +10,8 @@ const api = {
 function App() {
 
   const [images, setImages] = useState(); // array of JSON data returned from the API
-  const [selectedDate, setSelectedDate] = useState(new Date(new Date().getTime() - 2160000000).toLocaleDateString("en-CA")) // the default date to be used for the date picker
-  const [checkDate, setCheckDate] = useState(selectedDate) // the start_date parameter to be passed into the API
+  const [checkDate, setCheckDate] = useState(new Date(new Date().getTime() - 2160000000).toLocaleDateString("en-CA")) // the start_date parameter to be passed into the API
   const [isLoading, setIsLoading] = useState(true) // loading state
-  const dateRef = useRef() // reference to the date picker element
 
   // fetch data from API at the start of the website and anytime 'checkDate' changes
   useEffect(() => {
@@ -44,18 +42,6 @@ function App() {
   }, [checkDate])
 
 
-  function handleSubmit(e) {
-    e.preventDefault()
-
-    // condition to handle invalid date chosen on the mobile web
-    if (dateRef.current.value > dateRef.current.max || dateRef.current.value < dateRef.current.min) {
-      setSelectedDate(checkDate) // set date on Datepicker to most recent queried date
-      return alert(`APOD API cannot provide any images for this day: ${dateRef.current.value} \n\nPlease choose a date between ${dateRef.current.min} and ${dateRef.current.max}`)
-    }
-    setCheckDate(selectedDate)
-  }
-
-
   return (
     <div>
       <div className="App">
@@ -66,43 +52,23 @@ function App() {
         </Router>
 
         <p>Brought to you by NASA's Astronomy Photo of the Day (APOD) API</p>
-
-        <form>
-          <div className='label'>
-            <label htmlFor="datepicker">Choose starting date to display images from</label>
-          </div>
-          <div>
-            <input
-              ref={dateRef}
-              type="date"
-              id="datepicker"
-              value={selectedDate}
-              onChange={e => setSelectedDate(e.target.value)}
-              min="1995-06-16"
-              max={new Date().toLocaleDateString("en-CA")}
-            />
-          </div>
-          <div>
-            <button onClick={e => handleSubmit(e)}>Change Date</button>
-          </div>
-        </form>
-
-        <div className="dateinfo">
-          {(!isLoading && images) && <h3>Showing images from <strong>{checkDate}</strong></h3>}
-        </div>
       </div>
 
       {!isLoading && images
-        ? images.map(image => (
-          <Images images={image} key={image.date} />
-        )) :
+        ? <><div className="dateinfo">
+          <h3>
+            Showing images from <br />
+            <i>{checkDate} <strong>to</strong> {new Date().toLocaleDateString("en-CA")}</i>
+          </h3>
+        </div>
+          {images.map(image => (
+            <Images images={image} key={image.date} />
+          ))}</> :
         (!isLoading && !images ?
           <div className='dateinfo'>
             <h3>An error has occured. Please try again</h3>
           </div> : // INTERNAL SERVER ERROR HANDLING
-          <div className='dateinfo'>
-            <h3>Loading...</h3>
-          </div>
+          <div className='loading-state'></div>
         )}
     </div>
 
